ecfbd58ab381bd7ef39d316851b80633d46f8436
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-05-14 20:25:21 +0000

    adding entities files to the project

--- src/place/entities/place.entity.ts
+++ src/place/entities/place.entity.ts
@@ -1,7 +1,38 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@Entity()
+@ObjectType('Place')
 export class Place {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  description: string;
+
+  @Column()
+  @Field()
+  location: string;
+
+  @Column()
+  @Field()
+  latitude: string;
+
+  @Column()
+  @Field()
+  longitude: string;
+
+  @Column()
+  @Field()
+  open_hours: string;
+
+  @Column()
+  @Field()
+  createdAt: string;
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,22 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@Entity()
+@ObjectType('User')
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column({ unique: true })
+  @Field()
+  email: string;
+
+  @Column({ nullable: true, default: false })
+  @Field({ nullable: true })
+  is_admin: boolean;
+
+  @Column()
+  @Field()
+  password: string;
 }
