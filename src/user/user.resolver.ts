c4b45f6d19292012a793ae2f7e74252f18f9941c
Kwenzokuhle Dungulu <140886807+Dungul020@users.noreply.github.com>
2024-05-19 11:48:48 +0200

    Update user.resolver.ts
    Mutation to create a new user

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -5,6 +5,14 @@ import { User } from './entities/user.entity';
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) { }
+
+ @Mutation(() => User)
+  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.userService.create(createUserInput);
+  }
+
+
+  
   @Query(() => User)
   async user(@Args('email') email: string): Promise<User> {
     return this.userService.findOneByEmail(email);
